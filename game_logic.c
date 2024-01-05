2a92d4a3c9dde97eb015c624ff4e4de18c3c1443
ALESSANDRO FEDRIGA s320136 <s320136@studenti.polito.it>
2024-01-05 18:23:57 +0100

    added barriers check and various fixes

--- game_logic.c
+++ game_logic.c
@@ -18,63 +18,71 @@ typedef struct move {
 	uint8_t new_y;
 } Move;
 
-void paint_left_square(Pos position) {
-	paint_square(position.x-1, position.y, Yellow);
+void paint_left_square(uint8_t board[][BOARD_LENGTH], Pos position) {
+	if(board[2*position.x-1][2*position.y] == 0) {					// check barrier
+		paint_square(position.x-1, position.y, Yellow);
+	}
 }
 
-void paint_right_square(Pos position) {
-	paint_square(position.x+1, position.y, Yellow);
+void paint_right_square(uint8_t board[][BOARD_LENGTH], Pos position) {
+	if(board[2*position.x+1][2*position.y] == 0) {					// check barrier
+		paint_square(position.x+1, position.y, Yellow);
+	}
 }
 
-void paint_up_square(Pos position) {
-	paint_square(position.x, position.y+1, Yellow);
+void paint_up_square(uint8_t board[][BOARD_LENGTH], Pos position) {
+	if(board[2*position.x][2*position.y-1] == 0) {					// check barrier
+		paint_square(position.x, position.y-1, Yellow);
+	}
 }
 
-void paint_down_square(Pos position) {
-	paint_square(position.x, position.y-1, Yellow);
+void paint_down_square(uint8_t board[][BOARD_LENGTH], Pos position) {
+	if(board[2*position.x][2*position.y+1] == 0) {					// check barrier
+		paint_square(position.x, position.y+1, Yellow);
+	}
 }
 
-void Show_Possible_Moves(uint8_t board[][BOARD_LENGTH], Pos position) {
-	if((position.x != 0 && position.y != 0) || 
-		 (position.x != NUM_SQUARE_PER_SIDE-1 && position.y != NUM_SQUARE_PER_SIDE-1)) {	// not on the border
-		paint_left_square(position);
-		paint_right_square(position);
-		paint_up_square(position);
-		paint_down_square(position);
-	} else if(position.x == 0 && position.y == 0) {		// top left corner
-		paint_right_square(position);
-		paint_down_square(position);
+void Show_Possible_Moves(uint8_t board[][BOARD_LENGTH], Pos position) {	
+	if(position.x == 0 && position.y == 0) {		// top left corner
+		paint_right_square(board, position);
+		paint_down_square(board, position);
 	} else if(position.x == NUM_SQUARE_PER_SIDE-1 && position.y == 0) {		// top right corner
-		paint_left_square(position);
-		paint_down_square(position);
+		paint_left_square(board, position);
+		paint_down_square(board, position);
 	} else if(position.x == 0 && position.y == NUM_SQUARE_PER_SIDE-1) {		// bottom left corner
-		paint_up_square(position);
-		paint_right_square(position);
+		paint_up_square(board, position);
+		paint_right_square(board, position);
 	} else if(position.x == NUM_SQUARE_PER_SIDE-1 && position.y == NUM_SQUARE_PER_SIDE-1) {		// bottom right
-		paint_left_square(position);
-		paint_up_square(position);
-	} else if(position.x == 0) {		// top border 
-		paint_left_square(position);
-		paint_down_square(position);
-		paint_right_square(position);
-	} else if(position.x == NUM_SQUARE_PER_SIDE-1) {	// bottom border
-		paint_left_square(position);
-		paint_up_square(position);
-		paint_right_square(position);
-	} else if(position.y == 0) {	// left border
-		paint_up_square(position);
-		paint_right_square(position);
-		paint_down_square(position);
-	} else if(position.y == NUM_SQUARE_PER_SIDE-1) {	// right border
-		paint_up_square(position);
-		paint_left_square(position);
-		paint_down_square(position);
+		paint_left_square(board, position);
+		paint_up_square(board, position);
+	} else if(position.x == 0) {		// left border 
+		paint_up_square(board, position);
+		paint_down_square(board, position);
+		paint_right_square(board, position);
+	} else if(position.x == NUM_SQUARE_PER_SIDE-1) {	// right border
+		paint_left_square(board, position);
+		paint_up_square(board, position);
+		paint_down_square(board, position);
+	} else if(position.y == 0) {	// top border
+		paint_left_square(board, position);
+		paint_right_square(board, position);
+		paint_down_square(board, position);
+	} else if(position.y == NUM_SQUARE_PER_SIDE-1) {	// bottom border
+		paint_up_square(board, position);
+		paint_left_square(board, position);
+		paint_right_square(board, position);
+	} else if((position.x != 0 && position.y != 0) || 
+						(position.x != NUM_SQUARE_PER_SIDE-1 && position.y != NUM_SQUARE_PER_SIDE-1)) {	// not on the border
+		paint_left_square(board, position);
+		paint_right_square(board, position);
+		paint_up_square(board, position);
+		paint_down_square(board, position);
 	}
 }
 
 void update_board_player_move(uint8_t board[][BOARD_LENGTH], Move move) {
-	board[2*move.old_x+1][2*move.old_y+1] = 0;
-	board[2*move.new_x+1][2*move.new_y+1] = 1;
+	board[2*move.old_x][2*move.old_y] = 0;
+	board[2*move.new_x][2*move.new_y] = 1;
 }
 
 void update_board_barrier(uint8_t board[][BOARD_LENGTH], Position_barrier barrier) {
@@ -92,5 +100,5 @@ void Start_Game(void) {
 	init_timer(0, 0x00B71B00);
 	enable_timer(0);
 	
-	Show_Possible_Moves(board, player1);	
+	Show_Possible_Moves(board, player1);
 }
